01d8176ce9575a2663bb57c350beb2699039b15f
connglli <leetsong.lc@gmail.com>
2024-08-19 22:41:47 +0800

    Update license by adding congli

--- Sources/Fuzzilli/Engines/JITMutEngine.swift
+++ Sources/Fuzzilli/Engines/JITMutEngine.swift
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2024 Cong Li (congli@smail.nju.edu.cn, cong.li@inf.ethz.ch)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
